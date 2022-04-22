46b1d4e6622e5b65ef64e391577c22966b543e06
KokeHaus <austinbcw@gmail.com>
2022-04-22 13:30:58 -0500

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generateMarkdown.js');
+const path = require('path');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'How does one install your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How does one use your project?',
+      },
+    //   {
+    //     type: 'input',
+    //     name: 'license',
+    //     message: 'What licensing does your project have?',
+    //   },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'How does one contribute to your project?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'How does one test your project?',
+      },
+      {
+        type: "checkbox",
+        message: "What License did you use for this repository?",
+        choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"],
+        name: "license",
+    },
+     
+    ]).then((response) => {
+        return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+      });;
+  };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptUser();
